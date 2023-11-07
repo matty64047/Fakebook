577e28f45b7060be886e85bc696941f1988d9743
RobStarkie <rc.starkie@yahoo.co.uk>
2023-11-07 12:59:21 +0000

    loggin page -  typscript file and css file

--- fakebook/src/Login.tsx
+++ fakebook/src/Login.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Login.css';
 
-interface LoginProps {
+interface HomeProps {
   handleLogin: () => void;
 }
 
-const Login: React.FC<LoginProps> = ({ handleLogin }) => {
-  const handleLoginClick = () => {
-    // Simulating login, this could be replaced with actual login logic
-    handleLogin();
-  };
 
-  return (
-    <div>
-      <h1>Login Page</h1>
-      <button onClick={handleLoginClick}>Login</button>
-      {/* Add login form or other components for the Login screen */}
+export const Login: React.FC<HomeProps> = ({ handleLogin }) => {
+  const [username, setUsername] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+
+  return(
+    <div className="content">
+      <div className="flex-div">
+        <div className="name-content">
+          <h1 className="logo">Facebook</h1>
+          <p>Connect with friends and the world around you on Facebook.</p>
+        </div>
+        <form>
+          <input type="text" placeholder="Email or Phone Number" required onChange={e =>{
+            setUsername(e.target.value);
+            e.preventDefault();
+          }} />
+          <input type="password" placeholder="Password" required onChange={e => {
+            setPassword(e.target.value);
+            e.preventDefault();
+          }}/>
+          <button className="login">Log In</button>
+          <a href="#">Forgot Password ?</a>
+          <button onClick={handleLogin} className="create-account">Create New Account</button>
+        </form>
+      </div>
     </div>
-  );
-};
-
+  )
+}
 export default Login;
