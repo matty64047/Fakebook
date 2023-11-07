086f41072cbe4240e62f970e9984f3f008c910b6
Matt Connolly <54942633+matty64047@users.noreply.github.com>
2023-11-07 13:36:55 +0000

    Update Login.tsx

--- fakebook/src/Login.tsx
+++ fakebook/src/Login.tsx
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
 import './Login.css';
 
-interface HomeProps {
+interface LoginProps {
   handleLogin: () => void;
 }
 
-
-export const Login: React.FC<HomeProps> = ({ handleLogin }) => {
+const Login: React.FC<LoginProps> = ({ handleLogin }) => {
   const [username, setUsername] = useState<string | null>(null);
   const [password, setPassword] = useState<string | null>(null);
+
+  const handleLoginClick = () => {
+    handleLogin();
+  };
 
   return(
     <div className="content">
       <div className="flex-div">
         <div className="name-content">
-          <h1 className="logo">Facebook</h1>
-          <p>Connect with friends and the world around you on Facebook.</p>
+          <h1 className="logo">Fakebook</h1>
+          <p>Connect with friends and the world around you on Fakebook.</p>
         </div>
         <form>
           <input type="text" placeholder="Email or Phone Number" required onChange={e =>{
@@ -26,12 +29,13 @@ export const Login: React.FC<HomeProps> = ({ handleLogin }) => {
             setPassword(e.target.value);
             e.preventDefault();
           }}/>
-          <button className="login">Log In</button>
+          <button onClick={handleLoginClick} className="login">Log In</button>
           <a href="#">Forgot Password ?</a>
-          <button onClick={handleLogin} className="create-account">Create New Account</button>
+          <button className="create-account">Create New Account</button>
         </form>
       </div>
     </div>
   )
-}
+};
+
 export default Login;
