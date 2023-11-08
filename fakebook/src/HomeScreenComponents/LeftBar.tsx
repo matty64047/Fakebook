a4ca2bc2924bce0a090deb3f28e549067ff3358a
RobStarkie <rc.starkie@yahoo.co.uk>
2023-11-08 09:09:30 +0000

    ts and css files for home screen bars

--- fakebook/src/HomeScreenComponents/LeftBar.tsx
+++ fakebook/src/HomeScreenComponents/LeftBar.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import './LeftBar.css'
+
 
 interface LeftBarProps {
   content: string;
 }
 
+
 const LeftBar: React.FC<LeftBarProps> = ({ content }) => {
+  
   return (
-    <div className="mainColumn">
-      <p>{content}</p>
-    </div>
+    <body>
+      <div className="leftSidebar">
+        <ul>
+          <li><a className="active" href="#home">Home</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </div>
+    </body>
   );
 };
 
--- fakebook/src/Login.tsx
+++ fakebook/src/Login.tsx
@@ -14,27 +14,29 @@ const Login: React.FC<LoginProps> = ({ handleLogin }) => {
   };
 
   return(
-    <div className="content">
-      <div className="flex-div">
-        <div className="name-content">
-          <h1 className="logo">Fakebook</h1>
-          <p>Connect with friends and the world around you on Fakebook.</p>
+    <body>
+      <div className="content">
+        <div className="flex-div">
+          <div className="name-content">
+            <h1 className="logo">Facebook</h1>
+            <p>Connect with friends and the world around you on Facebook.</p>
+          </div>
+            <form>
+              <input type="text" placeholder="Email or Phone Number" required onChange={e =>{
+                setUsername(e.target.value);
+                e.preventDefault();
+              }} />
+              <input type="password" placeholder="Password" required onChange={e => {
+                setPassword(e.target.value);
+                e.preventDefault();
+              }}/>
+              <button className="login" onClick={handleLoginClick}>Log In</button>
+              <a href="#">Forgot Password ?</a>
+              <button className="create-account">Create New Account</button>
+            </form>
         </div>
-        <form>
-          <input type="text" placeholder="Email or Phone Number" required onChange={e =>{
-            setUsername(e.target.value);
-            e.preventDefault();
-          }} />
-          <input type="password" placeholder="Password" required onChange={e => {
-            setPassword(e.target.value);
-            e.preventDefault();
-          }}/>
-          <button onClick={handleLoginClick} className="login">Log In</button>
-          <a href="#">Forgot Password ?</a>
-          <button className="create-account">Create New Account</button>
-        </form>
       </div>
-    </div>
+    </body>
   )
 };
 
